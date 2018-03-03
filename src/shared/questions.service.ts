1e8baf994df2363900e420ef761aabaf2ecba2f5
MistyBlunch <gracenikole@hotmail.com>
2018-03-03 15:46:08 -0500

    :koala:changes update

---
+++ src/shared/questions.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class QuestionsService {
+
+    ref: AngularFireList<any>
+    
+
+}
